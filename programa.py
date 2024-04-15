7fc00928420ad075cfca936e912923938f6405c9
Paulo Coronado <paulocoronado@udistrital.edu.co>
2024-04-15 15:15:15 -0500

    v 1.0.0

--- programa.py
+++ programa.py
@@ -1,1 +1,33 @@
-#Taller Numpy
+#Numpy: Biblioteca para realizar cálculos numéricos en
+#arreglos n-dimensionales
+import numpy as np
+
+#Crear un ndarray de 2 dimensiones a partir de una lista
+A = np.array([[1,5], [7,9]])
+B = np.array([[2,0], [1,3]])
+#Producto punto entre ndarrays
+C= np.dot(A,B)
+print(C)
+#Solución de un sistema de ecuaciones con numpy
+m_solucion=np.array([5,17])
+m= np.linalg.solve(A,m_solucion)
+print(m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
